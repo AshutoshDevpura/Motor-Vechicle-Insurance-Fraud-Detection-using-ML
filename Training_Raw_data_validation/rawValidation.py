ecd49a678c412f1cd09df528ddf7367670ebeef6
AshutoshDevpura <ashutoshdevpura@gmail.com>
2022-07-26 16:31:47 +0530

    import required modules & libraries

---
+++ Training_Raw_data_validation/rawValidation.py
@@ -0,0 +1,12 @@
+
+# Author:Ashutosh Devpura
+# EmailId:ashutoshdevpura@gmail.com
+
+import sqlite3
+from datetime import datetime
+from os import listdir
+import os
+import re
+import json
+import shutil
+import pandas as pd
